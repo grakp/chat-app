aa76a13e91e02f1d0c67fd571c36d34197f65556
grakp <grace.zhao325@gmail.com>
2024-11-24 17:19:16 -0500

    initial db setup

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,11 +1,17 @@
 import express from "express";
+import dotenv from "dotenv";
 
+import { connectDB } from "./lib/db";
 import authRoutes from "./routes/auth.route";
 
 const app = express();
 
+dotenv.config();
+const PORT = process.env.PORT;
+
 app.use("/api/auth", authRoutes);
 
-app.listen(3000, () => {
-    console.log("Server is running on port 3000.");
+app.listen(PORT, () => {
+    console.log("Server is running on PORT: " + PORT);
+    connectDB();
 })
